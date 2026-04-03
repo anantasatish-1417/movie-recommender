c33e0809a5f4350d6d01abb1f1f96407487f14fb
Ananta Satish <cananthasatish@gmail.com>
2026-04-03 16:59:43 +0530

    final working version

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import streamlit as st
-import pickle
+import pandas as pd
 import requests
+import os
+import ast
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# ---------------- FETCH POSTER ----------------
 def fetch_poster(movie_id):
     try:
-        import os
         api_key = os.getenv("TMDB_API_KEY")
-
         url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
         data = requests.get(url, timeout=5).json()
 
         poster_path = data.get('poster_path')
 
-        if poster_path is not None:
+        if poster_path:
             return "https://image.tmdb.org/t/p/w500/" + poster_path
         else:
             return "https://via.placeholder.com/300x450?text=No+Poster"
@@ -19,31 +23,74 @@ def fetch_poster(movie_id):
     except:
         return "https://via.placeholder.com/300x450?text=Error"
 
-movies = pickle.load(open('movies.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
 
+# ---------------- LOAD & PROCESS DATA ----------------
+movies = pd.read_csv('tmdb_5000_movies.csv')
+credits = pd.read_csv('tmdb_5000_credits.csv')
+
+movies = movies.merge(credits, on='title')
+
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+movies.dropna(inplace=True)
+
+def convert(text):
+    return [i['name'] for i in ast.literal_eval(text)]
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+
+def convert_cast(text):
+    return [i['name'] for i in ast.literal_eval(text)[:3]]
+
+movies['cast'] = movies['cast'].apply(convert_cast)
+
+def fetch_director(text):
+    return [i['name'] for i in ast.literal_eval(text) if i['job'] == 'Director']
+
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+
+new_df = movies[['movie_id','title','tags']]
+
+new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x).lower())
+
+# ---------------- VECTORIZE ----------------
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(new_df['tags']).toarray()
+
+similarity = cosine_similarity(vectors)
+
+
+# ---------------- RECOMMEND FUNCTION ----------------
 def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
+    movie_index = new_df[new_df['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    movies_list = sorted(list(enumerate(distances)),
+                         reverse=True,
+                         key=lambda x: x[1])[1:6]
 
     recommended_movies = []
     recommended_posters = []
 
     for i in movies_list:
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movies.append(movies.iloc[i[0]].title)
+        movie_id = new_df.iloc[i[0]].movie_id
+        recommended_movies.append(new_df.iloc[i[0]].title)
         recommended_posters.append(fetch_poster(movie_id))
 
     return recommended_movies, recommended_posters
 
+
+# ---------------- STREAMLIT UI ----------------
 st.title("🎬 Movie Recommender System")
 st.markdown("### Get similar movies instantly 🍿")
 st.write("---")
 
 selected_movie = st.selectbox(
     "Select a movie",
-    movies['title'].values
+    new_df['title'].values
 )
 
 if st.button('Recommend'):
